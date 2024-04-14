26f29ad8af0616ffe1bc7d1c2bf3eb4cfb233894
johannpa <johann_paque@hotmail.fr>
2024-04-14 16:22:11 +0200

    Conditional rendering

--- src/Greatings.jsx
+++ src/Greatings.jsx
@@ -2,11 +2,16 @@ import React from "react";
 
 export function Greatings(props) {
   console.log("***", props);
-  return (
-    <ul>
-      <li>Hello {props.firstName}</li>
-      <li>you are {props.age} years old </li>
-      <li>{props.children}</li>
-    </ul>
-  );
+
+  if (props.age > 100) {
+    return <p>Tu n'es pas tout jeune</p>;
+  } else {
+    return (
+      <ul>
+        <li>Hello {props.firstName}</li>
+        <li>you are {props.age} years old </li>
+        <li>{props.children}</li>
+      </ul>
+    );
+  }
 }
