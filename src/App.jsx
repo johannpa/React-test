8b98b7fabe44180ee4387b6d8b62a30f151467b5
johannpa <johann_paque@hotmail.fr>
2024-04-14 18:32:09 +0200

    The way to make style inline

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,19 @@
 import React from "react";
-import { AgeCounter } from "./AgeCounter";
+// import { AgeCounter } from "./AgeCounter";
 // import { Greatings } from "./Greatings";
 
 export function App() {
   return (
     <>
-      <AgeCounter />
+      <div
+        style={{
+          backgroundColor: "red",
+          height: 100,
+          width: 100,
+          border: "3px solid blue",
+        }}
+      ></div>
+      {/* <AgeCounter /> */}
     </>
   );
 }
