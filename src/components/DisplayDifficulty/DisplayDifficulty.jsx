befd442a9abba343565912ac87b18886c94739dd
johannpa <johann_paque@hotmail.fr>
2024-04-17 20:46:18 +0200

    Import the style and create the container class

--- src/components/DisplayDifficulty/DisplayDifficulty.jsx
+++ src/components/DisplayDifficulty/DisplayDifficulty.jsx
@@ -1,5 +1,6 @@
 import React from "react";
+import style from "./style.module.css";
 
 export function DisplayDifficulty(props) {
-  return <div></div>;
+  return <div className={style.container}></div>;
 }
