250185152f4aeba7744f9fad3d43e8cdd27be6a9
johannpa <johann_paque@hotmail.fr>
2024-04-19 20:58:42 +0200

    MenuListItem takes props and a body

--- src/components/MenuListItem/MenuListItem.jsx
+++ src/components/MenuListItem/MenuListItem.jsx
@@ -2,5 +2,5 @@ import React from "react";
 import style from "./style.module.css";
 
 export function MenuListItem(props) {
-  return <div></div>;
+  return <div className={style.container}>Set to : {props.difficulty}</div>;
 }
