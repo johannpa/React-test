ff5e57f9dd374425de2752f2e1513edce74bcc47
johannpa <johann_paque@hotmail.fr>
2024-04-19 21:12:51 +0200

    Use a state and test it

--- src/components/MenuListItem/MenuListItem.jsx
+++ src/components/MenuListItem/MenuListItem.jsx
@@ -1,6 +1,16 @@
-import React from "react";
+import React, { useState } from "react";
 import style from "./style.module.css";
 
 export function MenuListItem(props) {
-  return <div className={style.container}>Set to : {props.difficulty}</div>;
+  const [isHovered, setIsHovered] = useState(false);
+  console.log("***", isHovered);
+  return (
+    <div
+      className={style.container}
+      onMouseEnter={() => setIsHovered(true)}
+      onMouseLeave={() => setIsHovered(false)}
+    >
+      Set to : {props.difficulty}
+    </div>
+  );
 }
