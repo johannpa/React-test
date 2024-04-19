aed50b4c271ada62e0721561c03b686abc4ea7d1
johannpa <johann_paque@hotmail.fr>
2024-04-19 21:02:28 +0200

    Add the new component to App

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,14 @@
 import React from "react";
 import { DisplayDifficulty } from "./components/DisplayDifficulty/DisplayDifficulty";
+import { MenuListItem } from "./components/MenuListItem/MenuListItem";
 
 export function App() {
   return (
     <>
-      <div>{/* <DisplayDifficulty difficulty="Low" /> */}</div>
+      <div>
+        {/* <DisplayDifficulty difficulty="Low" /> */}
+        <MenuListItem difficulty="Low" />
+      </div>
     </>
   );
 }
