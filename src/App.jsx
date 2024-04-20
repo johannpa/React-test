daea815a25324718907185d96af1bd09e9d83b83
johannpa <johann_paque@hotmail.fr>
2024-04-20 17:40:48 +0200

    Create a style.module.css for App, add a title for App and put the MenuList component with the DisplayDifficulty component in the same line

--- src/App.jsx
+++ src/App.jsx
@@ -2,6 +2,7 @@ import React, { useState } from "react";
 import { DisplayDifficulty } from "./components/DisplayDifficulty/DisplayDifficulty";
 // import { MenuListItem } from "./components/MenuListItem/MenuListItem";
 import { MenuList } from "./components/MenuList/MenuList";
+import style from "./style.module.css";
 
 export function App() {
   const [currentDifficulty, setCurrentDifficulty] = useState("");
@@ -12,11 +13,14 @@ export function App() {
   return (
     <>
       <div>
-        <MenuList
-          onItemClick={updateDifficulty}
-          difficulty={currentDifficulty}
-        />
-        <DisplayDifficulty difficulty={currentDifficulty} />
+        <h1 className={style.title}>Difficulty Selector</h1>
+        <div className={style.workspace}>
+          <MenuList
+            onItemClick={updateDifficulty}
+            difficulty={currentDifficulty}
+          />
+          <DisplayDifficulty difficulty={currentDifficulty} />
+        </div>
       </div>
     </>
   );
